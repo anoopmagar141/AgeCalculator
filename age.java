374d5ec502dde2b2d6b3ea5385ec14a2ff5f820a
[Anup  Magar] <am6956275@gmail.com>
2025-03-15 22:18:24 +0545

    main function

--- age.java
+++ age.java
@@ -3,3 +3,13 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
+
+public class age {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your birthdate (YYYY-MM-DD, YYYY/MM/DD, YYYY.MM.DD): ");
+        String inputDate = scanner.nextLine();
+
+        scanner.close();
+    }
+}
